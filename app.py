f95b7f2f8ad12cbcf055eb04ab81139b6b16d758
Ben Cho <ben.cho@gmail.com>
2022-12-13 23:01:11 -0700

    added link to help

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import data
 
+# https://docs.streamlit.io/library/api-reference?highlight=beta_page_config#streamlit.set_page_config
+
 # st.set_page_config(page_title='your_title', layout = 'wide', initial_sidebar_state = 'auto') #page_icon = favicon, 
 st.set_page_config(layout = 'wide')
 
